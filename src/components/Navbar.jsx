743e7b71ba6620d13f15969c940d5da3da75ae06
Elturan <elturanfcb@gmail.com>
2023-07-21 15:18:24 +0400

    navbarda user gorunur ve search isleyir

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { signOut } from "firebase/auth"
 import { auth } from "../firebase"
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
 
 export default function Navbar() {
+  const { currentUser } = useContext(AuthContext)
   return (
     <div className='navbar'>
       <span className='logo'>Çat App</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/17427589/pexels-photo-17427589/free-photo-of-portrait-of-a-male-model-wearing-a-black-jacket-and-white-shoes.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-        <span>Elturan</span>
+        <img src={currentUser.photoURL} alt="photoURL" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Çıxış</button>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react"
+import { collection, doc, getDocs, query, where } from "firebase/firestore"
+import { db } from "../firebase"
+
 export default function Search() {
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState()
+  const [err, setErr] = useState(false)
+
+  const handleSearch = async () => {
+    const usersRef = collection(db, 'users')
+    const q = query(usersRef, where("displayName", "==", username))
+
+    try {
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      })
+    } catch (error) {
+      setErr(true)
+    }
+  }
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch()
+  }
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='İstifadəçi axtar' />
+        <input type="text" placeholder='İstifadəçi axtar' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)} />
       </div>
-      <div className="userChat">
-        <img src="https://images.pexels.com/photos/17427589/pexels-photo-17427589/free-photo-of-portrait-of-a-male-model-wearing-a-black-jacket-and-white-shoes.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="userImage" />
-        <div className="userChatInfo">
-          <span>Nicat</span>
+      {
+        user && <div className="userChat">
+          <img src={user.photoURL} alt="userImage" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
